1b6786dc3b8a079b66a1076fc9e0b6b99d89adcc
lucasFilppe <lucasFilipe2323 @icloud.com>
2023-07-06 17:46:07 -0300

    alocacao dinamica: 6.c

--- 1-alocacao-dinamica/6.c
+++ 1-alocacao-dinamica/6.c
@@ -2,9 +2,49 @@
 func¸ao para alocar dinamicamente essa string. Em seguida, o usuario devera informar o ´
 conteudo dessa string. O programa imprime a string sem suas vogais. */
 #include <stdio.h>
-#include<stdlib.>
+#include<stdlib.h>
+#include<string.h>
 
-int main(){
+
+// Função que aloca uma string
+char* alocaString(int tamanho) {
+  char* s = (char*)malloc((tamanho + 1) * sizeof(char)); // +1 para o caractere nulo '\0'
+
+  // Lê a string
+  printf("Digite a string: ");
+  fgets(s, tamanho + 1, stdin);
+
+  return s;
+}
+
+// Função que remove as vogais da string
+void removeVogais(char* str) {
+  int i, j;
+  for (i = 0, j = 0; str[i] != '\0'; i++) {
+    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
+        str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
+      str[j++] = str[i];
+    }
+  }
+  str[j] = '\0'; // Adiciona o caractere nulo '\0' no final da string resultante
+}
+
+int main() {
+  int n;
+
+  printf("Digite o tamanho da string: ");
+  scanf("%d", &n);
+
+  // Limpa o buffer do teclado
+  while (getchar() != '\n');
+
+  char* palavra = alocaString(n);
+
+  removeVogais(palavra);
+
+  printf("String sem vogais: %s\n", palavra);
+
+  free(palavra); // Libera a memória alocada
 
   return 0;
 }
